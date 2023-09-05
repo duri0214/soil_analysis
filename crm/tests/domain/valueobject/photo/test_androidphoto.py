bbe10097908ecb962e6620f9aeb9bdbcfd79a932
yoshi <yoshitakaOkada0214@gmail.com>
2023-09-05 23:11:56 +0900

    Lavieの写真テストに変更（結局azimuthはなさそう）

--- crm/tests/domain/valueobject/photo/test_androidphoto.py
+++ crm/tests/domain/valueobject/photo/test_androidphoto.py
@@ -6,13 +6,13 @@ from crm.domain.valueobject.photo.androidphoto import AndroidPhoto
 
 class TestAndroidPhoto(TestCase):
     def setUp(self):
-        self.file_path = r"D:/OneDrive/dev/soil_analysisローカルデータ/サンプルデータ/android/ススムＢ1_right.jpg"
+        self.file_path = r"D:/OneDrive/dev/soil_analysisローカルデータ/サンプルデータ/android/JA中_all.jpg"
         self.android_photo = AndroidPhoto(self.file_path)
         self.android_photo.exif_data = self.android_photo._extract_exif_data()
 
     def test_extract_date(self):
         # 正常な値のテスト
-        expected_date = "2023-06-18"
+        expected_date = "2023-08-26"
         actual_date = self.android_photo._extract_date()
         self.assertEqual(expected_date, actual_date)
 
@@ -23,9 +23,12 @@ class TestAndroidPhoto(TestCase):
 
     def test_extract_location(self):
         # 正常な値のテスト
-        expected_location = CaptureLocation(137.6494111111111, 34.745)
+        expected_location = CaptureLocation(137.8266552, 34.6942567)  # xarvio based
         actual_location = self.android_photo._extract_location()
-        self.assertEqual(expected_location.corrected.get_coords(), actual_location.corrected.get_coords())
+        self.assertAlmostEqual(expected_location.corrected.get_coords()[0], actual_location.corrected.get_coords()[0],
+                               delta=0.001)
+        self.assertAlmostEqual(expected_location.corrected.get_coords()[1], actual_location.corrected.get_coords()[1],
+                               delta=0.001)
 
         # GPS GPSLongitude が None の場合のテスト
         self.android_photo.exif_data["GPS GPSLongitude"] = None
